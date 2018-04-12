2e30f1f760e6522848766508aef170bcf7e05ca2
wannabewize <wannabewize@gmail.com>
2018-04-13 04:26:30 +0900

    뷰와 씬 작성, IBOutlet 연결 콘텐트 출력

--- AirCnC/AirCnC/ViewController.swift
+++ AirCnC/AirCnC/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var depthLabel: UILabel!
+    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        imageView.image = UIImage.init(named: "hattefjall")
+        
+        titleLabel.text = "HATTEFJÄLL"
+        
+        userImageView.image = #imageLiteral(resourceName: "jungyeon")
+        userNameLabel.text = "정연"
+        
+        priceLabel.text = "10,000"
+        depthLabel.text = "68cm"
+        widthLabel.text = "68cm"
+        heightLabel.text = "110cm"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
